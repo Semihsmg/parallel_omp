ad5631b6f6e67cbb6aec769caad7d4ae78a6abd8
Semih <semihsolmazgul@gmail.com>
2016-12-17 23:35:27 +0300

    Update aa.c

--- aa/src/aa.c
+++ aa/src/aa.c
@@ -9,41 +9,71 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_LINE_NUMBER 1000
-#define MAX_WORD_NUMBER 1000
+#define MAX_LINE 1000
 
 int main(int argc, char *argv[]) {
-	clock_t begin = clock();
-	char *fileDir = "C:\\Users\\ogulcan\\Paralel\\text.txt";
-	char *resultDir = "C:\\Users\\ogulcan\\Paralel\\result.txt";
+		char result[MAX_LINE][8];
+		char data[MAX_LINE][8];
+		char dictionary[2][8];
+		strcpy(dictionary[0],"semih");
+		strcpy(dictionary[1],"ogulcan");
 
-	FILE *txtFile = fopen(fileDir, "r"); // open as read only
-	FILE *txtResult = fopen(resultDir, "w"); // open as read and write
+		srand(time(NULL));
+		int i;
 
-	printf("File directory found: %s\n", fileDir);
-	int i = 0;
-	char words[MAX_LINE_NUMBER][MAX_WORD_NUMBER];
-	char * prt;
-
-	while (i < MAX_LINE_NUMBER && !feof(txtFile)) {
-
-		fgets(words[i], MAX_WORD_NUMBER, txtFile);
-		prt = strtok(&words[i], "' ','\n'");
-		while (prt != NULL) {
-			printf("%s\n", prt);
-			fputs(prt, txtResult);
-			putc('\n', txtResult);
-			prt = strtok(NULL, "' ','\n'");
+		for (i = 0; i < MAX_LINE; i++) {
+			int r = (rand() % 5);
+			if (r == 0) {
+				strcpy(data[i], "semih");
+			} else if (r == 1) {
+				strcpy(data[i], "semihd");
+			} else if (r == 2) {
+				strcpy(data[i], "ogulcan");
+			} else if (r == 3) {
+				strcpy(data[i], "ogulcand");
+			} else {
+				strcpy(data[i], "asdasd");
+			}
 		}
-		i++;
+
+	int j,k, r = 0, tid;
+
+#pragma omp parallel private(i,j,k,r,tid)
+	{
+
+		tid = omp_get_thread_num();
+
+		int dist = MAX_LINE / omp_get_num_threads();
+		printf("dist:%d", dist);
+
+		for (i = 0; (tid * dist) + i < (dist * tid) + dist; i++) {
+
+			for(k=0;k<2;k++){
+
+			for (j = 0; j < 8; j++) {
+
+				if (data[(tid * dist) + i][j] == dictionary[k][j]) {
+					r++;
+				}
+
+
+			if (r > 3 ) {
+
+				strcpy(result[(tid * dist) + i], dictionary[k]);
+
+			} else if(strlen(result[(tid * dist) + i])<5){
+				strcpy(result[(tid * dist) + i], data[(tid * dist) + i]);
+			}
+
+			}r = 0;
+		}}
+
 	}
+	for (i = 0; i < MAX_LINE; i++) {
+		printf("result %d = %s\n", i, result[i]);
 
-	fclose(txtFile);
-	fclose(txtResult);
-
-	clock_t end = clock();
-	double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
-	printf("\n %f = timespent", time_spent);
+	}
 
 	return 0;
 }
+
